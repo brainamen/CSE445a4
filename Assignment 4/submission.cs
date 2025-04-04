b5fee7ce1868449d3fa4e3c8161206da2e296f22
Brian Amen <bramen121101@gmail.com>
2025-04-03 20:37:04 -0700

    Add project files.

---
+++ Assignment 4/submission.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Xml.Schema;
+using System.Xml;
+using Newtonsoft.Json;
+using System.IO;
+
+
+
+/**
+ * This template file is created for ASU CSE445 Distributed SW Dev Assignment 4.
+ * Please do not modify or delete any existing class/variable/method names. However, you can add more variables and functions.
+ * Uploading this file directly will not pass the autograder's compilation check, resulting in a grade of 0.
+ * **/
+
+
+namespace ConsoleApp1
+{
+
+
+    public class Program
+    {
+        public static string xmlURL = "http://www.w3.org/2001/XMLSchema-instance";
+        public static string xmlErrorURL = "http://www.w3.org/2001/XMLSchema-instance";
+        public static string xsdURL = "http://www.example.com/Hotels";
+
+        public static void Main(string[] args)
+        {
+            string result = Verification(xmlURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Verification(xmlErrorURL, xsdURL);
+            Console.WriteLine(result);
+
+
+            result = Xml2Json(xmlURL);
+            Console.WriteLine(result);
+        }
+
+        // Q2.1
+        public static string Verification(string xmlUrl, string xsdUrl)
+        {
+            //create a xml reader setting with the xsd schema
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(null, xsdUrl);
+            settings.ValidationType = ValidationType.Schema;
+            //create a xml reader to read the xml file
+            using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
+            {
+                try
+                {
+                    //read the xml file and validate against the xsd schema
+                    while (reader.Read())
+                    {
+
+                    }
+                    return "No error";
+                }
+                catch(XmlSchemaValidationException exception)
+                {
+                    return exception.Message;
+                }
+                catch(Exception exception)
+                {
+                    return exception.Message;
+                }
+            }
+            
+        }
+
+        public static string Xml2Json(string xmlUrl)
+        {
+            //load the xml file
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(xmlUrl);
+            //convert the xml document to json
+            string jsonText = JsonConvert.SerializeXmlNode(xmlDocument, Newtonsoft.Json.Formatting.Indented, true);
+
+            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
+            return jsonText;
+
+        }
+    }
+
+}
